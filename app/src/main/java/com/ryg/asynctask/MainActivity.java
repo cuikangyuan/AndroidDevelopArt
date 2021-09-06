95dcddd8581903e18071fcbe204ca8d665d19ea8
cuikangyuan <cuikangyuan@jd.com>
2021-09-06 16:47:26 +0800

    test

---
+++ app/src/main/java/com/ryg/asynctask/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ryg.asynctask;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ryg.chapter_2.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
